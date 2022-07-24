44913350a3a84c24900a3271d879bc117c34cb8c
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-07-24 13:12:36 +0700

    add: User e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import { AuthDto } from '../src/auth/dto';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { EditUserDto } from '../src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -113,7 +114,24 @@ describe('App e2e', () => {
       });
     });
 
-    describe('Edit User', () => {});
+    describe('Edit User', () => {
+      it('Should Edit User', () => {
+        const dto: EditUserDto = {
+          firstName: 'Tono',
+          email: 'tono@mgkusumaputra.my.id',
+        };
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.firstName)
+          .expectBodyContains(dto.email);
+      });
+    });
   });
 
   describe('bookmarks', () => {
